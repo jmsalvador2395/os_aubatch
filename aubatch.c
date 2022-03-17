9acbef09cf01879af6103288ee43a35a255e7be6
john <jmsalvador2395@gmail.com>
2022-03-16 23:29:44 -0500

    working on adding jobs to the job queue

--- aubatch.c
+++ aubatch.c
@@ -14,14 +14,20 @@
 #define MIN_CPU_TM	3
 #define MAX_CPU_TM	4
 
+#define FL_TERM		0x80
+#define FL_SCHED	0x01
+#define FL_RESCHED	0x02
+
 
 
 pthread_mutex_t policy_lock;
 pthread_mutex_t jq_lock;
-pthread_mutex_t mutex;
 
-pthread_cond_t jq_not_full;
-pthread_cond_t jq_not_empty;
+pthread_cond_t sched_cv;
+pthread_cond_t disp_cv;
+
+unsigned char sched_flags=0;
+unsigned char disp_flags=0;
 
 int terminate=0;
 int test=0;
@@ -40,50 +46,63 @@ void *dispatch_thread(){
 		pthread_mutex_lock(&jq_lock);
 
 		//wait for new job or termination request
-		while (test == 0 && !terminate){
-			pthread_cond_wait(&jq_not_empty, &jq_lock);
+		while (test == 0 && !disp_flags){
+			pthread_cond_wait(&disp_cv, &jq_lock);
 		}
 		
 		//check for termination request
-		if (terminate){
-			pthread_cond_signal(&jq_not_full);
+		if (disp_flags & FL_TERM){
+			pthread_cond_signal(&sched_cv);
 			pthread_mutex_unlock(&jq_lock);
 			break;
 		}
 
-		test--;
+		//test--;
 		//printf("dispatcher decremented\n");
 
 		//signal scheduler and release mutex
-		pthread_cond_signal(&jq_not_full);
+		disp_flags=0x00;
+		pthread_cond_signal(&sched_cv);
 		pthread_mutex_unlock(&jq_lock);
 	}
-
 }
+
+/*
+ * puts the submitted jobs into the job queue
+ */
 void *scheduler_thread(){
 	while(1){
 		pthread_mutex_lock(&jq_lock);
 
 		//wait for job queue to have space
-		while (test == 10 && !terminate){
-			pthread_cond_wait(&jq_not_full, &jq_lock);
+		while (test == 10 && !sched_flags){
+			pthread_cond_wait(&sched_cv, &jq_lock);
 		}
 
 		//check for termination request
-		if (terminate){
-			pthread_cond_signal(&jq_not_empty);
+		if (sched_flags & FL_TERM){
+			pthread_cond_signal(&disp_cv);
 			pthread_mutex_unlock(&jq_lock);
 			break;
 		}
+		else if(sched_flags & FL_SCHED){
+			printf("schedule called\n");
+			createq_job("job1", 2, 1);
+			print_jobq();
+			sched_flags=0x00;
+		}
+		else if (sched_flags & FL_RESCHED){
+			printf("reschedule called");
+			sched_flags=0x00;
+		}
 
-		test++;
+		//test++;
 		//printf("scheduler incremented\n");
 
 		//signal dispatcher and release mutex
-		pthread_cond_signal(&jq_not_empty);
+		pthread_cond_signal(&disp_cv);
 		pthread_mutex_unlock(&jq_lock);
 	}
-
 }
 
 int main(int argc, char **argv){
@@ -103,7 +122,7 @@ int main(int argc, char **argv){
 	int dispatch_res=pthread_create(&dispatcher, NULL, *dispatch_thread, NULL);
 	int scheduler_res=pthread_create(&scheduler, NULL, *scheduler_thread, NULL);
 
-	//main loop
+	//enter the main loop
 	int result;
 	int input_len=50;
 	char input[input_len];
@@ -132,7 +151,10 @@ int main(int argc, char **argv){
 				pthread_mutex_lock(&policy_lock);
 				policy="fcfs";
 				pthread_mutex_unlock(&policy_lock);
-				reschedule_jobs(policy);
+
+				sched_flags=FL_RESCHED;
+
+				pthread_cond_signal(&sched_cv);
 				printf("\n**policy changed to fcfs. job queue rescheduled\n\n");
 				break;
 			//sjf
@@ -140,7 +162,10 @@ int main(int argc, char **argv){
 				pthread_mutex_lock(&policy_lock);
 				policy="sjf";
 				pthread_mutex_unlock(&policy_lock);
-				reschedule_jobs(policy);
+
+				sched_flags=FL_RESCHED;
+
+				pthread_cond_signal(&sched_cv);
 				printf("\n**policy changed to sjf. job queue rescheduled\n\n");
 				break;
 			//priority
@@ -148,7 +173,10 @@ int main(int argc, char **argv){
 				pthread_mutex_lock(&policy_lock);
 				policy="priority";
 				pthread_mutex_unlock(&policy_lock);
-				reschedule_jobs(policy);
+
+				sched_flags=FL_RESCHED;
+
+				pthread_cond_signal(&sched_cv);
 				printf("\n**policy changed to priority. job queue rescheduled\n\n");
 				break;
 			//test
@@ -159,14 +187,23 @@ int main(int argc, char **argv){
 			case 6:
 				printf("\n**exiting aubatch ...\n\n");
 				//wait for threads to finish
-				terminate=1;
-				pthread_cond_signal(&jq_not_empty);
-				pthread_cond_signal(&jq_not_full);
+				sched_flags=FL_TERM;
+				disp_flags=FL_TERM;
+
+				pthread_cond_signal(&disp_cv);
+				pthread_cond_signal(&sched_cv);
+
 				pthread_join(dispatcher, NULL);
 				pthread_join(scheduler, NULL);
+				free_jobq();
+				print_jobq();
 				return 0;
 			case 7:
 				printf("\n**run job ...\n\n");
+				sched_flags=FL_SCHED;
+
+				pthread_cond_signal(&disp_cv);
+				pthread_cond_signal(&sched_cv);
 				break;
 			default:
 				break;
@@ -219,10 +256,11 @@ int get_case(char *input, int len){
 	else if (strstr(input, "test")){
 		return 5;
 	}
-	//TODO make a more graceful exit routine
+	//check if exit reoutine is called
 	else if (strcmp(input, "quit\n") == 0){
 		return 6;
 	}
+	//check if job is submitted
 	else if (strstr(input, "run")){
 		return 7;
 	}
